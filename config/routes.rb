ae6a93f0d63882a064f1a9f14a8b4ab2e13cca53
Jorge Chartier <jorgeandreschartier@gmail.com>
2012-07-15 00:53:57 -0300

    probando bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RailsinstallerOsxDemo::Application.routes.draw do
-  get "welcome/index"
+  get "layouts/application"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ RailsinstallerOsxDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'layouts#application'
 
   # See how all your routes lay out with "rake routes"
 
